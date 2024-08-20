4a5b4f3529d9c73312d4b12d8759f9aa2b3e21a3
Baski <victoria989876@gmail.com>
2024-08-19 22:12:12 -0600

    Solución al código en clase

--- codigo_clase/V_Argumentos_en_CMD.c
+++ codigo_clase/V_Argumentos_en_CMD.c
@@ -7,10 +7,14 @@ int main(int argc, char *argv[]){
     int i,j;
     char *num = argv[1];
 
-    for (j=0; j<= argc; j++)
-        fprintf(stdout, "arg[%d] = %c \n", j, num[j]);
+    //Para recorrer TODOS los argumentos del programa
+    for (j=0; j< argc; j++)
+        fprintf(stdout, "arg[%d] = %s \n", j, argv[j]);
+
+    //Imprime el PRIMER argumento después del nombre
     //fprintf(stdout, "*argv[1] = %s \n", argv[1]);
 
+    //Recorre caracter a caracter del PRIMER argumento
     for(i=0; i < strlen(argv[1]); i++)
         fprintf(stdout, "num[%d] = %c \n", i, num[i]);
 
