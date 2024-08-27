2b809fdbf0b3d94ecca1d0f8e7f45bc314cd13a9
DEVELOPMENT\Felipe <vsquezfelipe@gmail.com>
2024-08-26 20:59:16 -0600

    Finalizado!

--- tareas/programas/matrices y arreglos/PT4/Actividad_0.c
+++ tareas/programas/matrices y arreglos/PT4/Actividad_0.c
@@ -7,9 +7,10 @@ int main(int argc, char const *argv[])
 	
 	//Tamaño del arreglo
 	int T = 10;
+	int TC = 9;
 
 	int *oso = (int*) malloc(T * sizeof(int));
-	int *cuenta = (int*) malloc(T * sizeof(int));
+	int *cuenta = (int*) malloc(TC * sizeof(int));
 	/*
 	printf("oso = %p\n",oso);
 	printf("oso = %p\n",oso + 1);
@@ -22,20 +23,41 @@ int main(int argc, char const *argv[])
 		oso[i] = (i +2) + i;
 	}
 
+	for(int i = 0; i < TC; ++i)
+	{
+		cuenta[i] = atoi(argv[i + 1]);
+	}
+
 	//printf("Tam: %d\n",sizeof(oso)/sizeof(int));
 	//printf("oso[0] = %d",*(oso + 0));
 
 	printf("\nArreglo inicial:\n\n\t");
 	for(int i = 0; i < T; i++)
 	{
-		printf("| %d ",oso[i]);
+		printf("| %2d ",oso[i]);
+	}
+	printf("|\n");
+
+	printf("\nArreglo invertido:\n\n\t");
+	for(int i = T - 1; i >= 0; i--)
+	{
+		printf("| %2d ",oso[i]);
 	}
 	printf("|\n");
 
 	printf("\nNumero cuenta:\n\n\t");
-	for(int i = 0; i < T; i++)
+	for(int i = 0; i < TC; i++)
 	{
-		printf("| %d ",cuenta[i]);
+		printf("| %2d ",cuenta[i]);
 	}
 	printf("|\n");
+
+	printf("\nArreglo modificado (Posicion %d modificada):\n\n\t",cuenta[TC - 1]);
+	oso[cuenta[TC - 1]] = -1;
+	for(int i = 0; i < T; i++)
+	{
+		printf("| %2d ",oso[i]);
+	}
+	printf("|\n");
+
 }
